24143f6a66ee21ee87f06a94af80d233551d09f2
songbbang <rokaf340@gmail.com>
2023-07-31 17:12:10 +0900

    mongoose

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+/* eslint-disable */
 const express = require('express');
 const app = express();
 const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://rokaf340:rokaf340`@front.f5nk6u6.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+
+      // 에러방지
+    }
+  )
+  .then(() => {
+    console.log('mongodb connected...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get('/', (req, res) => res.send('hello world!'));
 app.listen(port, () => console.log(`example app listening on ${port}`));
